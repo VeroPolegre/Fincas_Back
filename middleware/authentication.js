6f5138c78955347b8c6c87178d7d37f11aff7aa5
VeroPolegre <veronicapolegre98@gmail.com>
2024-01-08 17:23:14 +0100

    mvc structure and authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwt_secret = process.env.JWT_SECRET;
+
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token, jwt_secret);
+    const user = await User.findOne({ _id: payload._id, tokens: token });
+    if (!user) {
+      return res.status(404).send({ message: "Not authorized" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(404)
+      .send({ message: "There was a problem with the token" });
+  }
+};
+
+const isAdmin = async (req, res, next) => {
+  const admins = ["admin", "superadmin"];
+  if (!admins.includes(req.user.role)) {
+    return res.status(403).send({ message: `You don't have permission` });
+  }
+  next();
+};
+
+module.exports = {
+  authentication,
+  isAdmin,
+};
